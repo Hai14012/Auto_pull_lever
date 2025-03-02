71cf7d03233c7b0c68a1e4d05d33544fde5b3211
Hai14012 <tranngoctruongtb1978@gmail.com>
2025-03-02 10:03:34 +0700

    Update mirage.lua at 1740884613

--- mirage.lua
+++ mirage.lua
@@ -1,1 +1,1 @@
-game:GetService("ReplicatedStorage").__ServerBrowser:InvokeServer("teleport", "ee040537-4e06-4a9c-816d-d53b82592587")
+game:GetService("ReplicatedStorage").__ServerBrowser:InvokeServer("teleport", "7a592e06-d5b5-475d-9d02-400df1c626ed")
